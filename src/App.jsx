6e8956742dcfe8afc8944aa30b713b9f103b53ad
Ayan <ayan2572006@gmail.com>
2024-06-10 16:23:44 +0530

    ball bounce animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
-import { useRef } from "react";
+
 gsap.registerPlugin(useGSAP);
 const App = () => {
-  const container = useRef();
+
   useGSAP(
     () => {
-        // ✅ safe, created during execution, selector text scoped
-        gsap.to('.box', { x: 100 });
-    },
-    { scope: container }
+      gsap.set("svg", { opacity: 1 });
+
+gsap.to(".ball", {
+  keyframes: {
+    "0%": { yPercent: 0, scaleX: 1, scaleY: 1 },
+    "7%": { yPercent: 5, scaleY: 0.9, scaleX: 1.1, ease: "sine.in" },
+    "25%": { yPercent: 100, scaleY: 1.15, scaleX: 0.9, ease: "sine.in" },
+    "50%": { yPercent: 500, scaleX: 1, scaleY: 1, ease: "none" },
+    "60%": { scaleX: 1.6, scaleY: 0.4, ease: "none" },
+    "65%": { yPercent: 500, scaleX: 1, scaleY: 1 },
+    "100%": { yPercent: 0, scaleX: 1, scaleY: 1 },
+    easeEach: "sine.out"
+  },
+  duration: 1.5,
+  repeat: -1,
+  transformOrigin: "center bottom"
+});
+
+gsap.to(".shadow", {
+  scale: 0.7,
+  duration: 0.4,
+  repeat: -1,
+  yoyo: true,
+  ease: "sine.inOut",
+  transformOrigin: "center"
+});
+    }
 );
-const onClick = () => {
-  gsap.to('.box', { duration:2.5,
-    ease: "bounce.out",
-    y: -250 });
-}
+
 
   return (
   <>
-<div ref={container} className=" flex items-center justify-center h-screen flex-col ">
-<div className="box h-[4rem] w-[4rem] bg-red-300"></div>
-<button className="rotate bg-black text-white p-1 mt-5 rounded" onClick={onClick}>Rotate</button>
+<div className="h-[100vh] w-[100vw] flex justify-center items-center overflow-hidden">
+<svg viewBox="0 0 100 200" className="w-[70vw]max-w-[500px] max-h-[80vh] opacity-0">
+  <ellipse className="shadow" cx="50" cy="188" rx="15" ry="5" />
 
+  <circle className="ball text-[#88ce02]" cx="50" cy="22" r="15" />
+</svg>
 </div>
 
   </>
